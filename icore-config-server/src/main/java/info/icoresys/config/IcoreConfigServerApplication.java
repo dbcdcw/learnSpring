0281113173ea1898867a53a90ec2341eca8675a0
dbc <chengwei.duan@cyberplus.com.cn>
2017-12-18 11:49:25 +0800

    config eureka

---
+++ icore-config-server/src/main/java/info/icoresys/config/IcoreConfigServerApplication.java
@@ -0,0 +1,12 @@
+package info.icoresys.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcoreConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcoreConfigServerApplication.class, args);
+	}
+}
---
+++ icore-eureka-server/src/main/java/info/icoresys/eureka/IcoreEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package info.icoresys.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcoreEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcoreEurekaServerApplication.class, args);
+	}
+}
