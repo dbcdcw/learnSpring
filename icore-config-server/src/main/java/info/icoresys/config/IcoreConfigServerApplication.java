51093f0458329b8993b6b459ec59d30581bdd755
dbc <chengwei.duan@cyberplus.com.cn>
2017-12-19 18:34:31 +0800

    添加测试\修改pom

--- icore-config-server/src/main/java/info/icoresys/config/IcoreConfigServerApplication.java
+++ icore-config-server/src/main/java/info/icoresys/config/IcoreConfigServerApplication.java
@@ -2,11 +2,15 @@ package info.icoresys.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class IcoreConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IcoreConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IcoreConfigServerApplication.class, args);
+    }
 }
